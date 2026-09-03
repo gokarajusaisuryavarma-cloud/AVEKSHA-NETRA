99aec3c9d79d94dd2aca29c6d42ac1b83493725a
Sai Surya Varma Gokaraju <saisuryavarmagokaraju007@gmail.com>
2026-09-03 07:21:06 +0530

    Fix production backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -69,12 +69,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-
         "http://localhost:5175",
         "http://127.0.0.1:5175",
+
+        "https://avekshanetra.in",
+        "https://www.avekshanetra.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
